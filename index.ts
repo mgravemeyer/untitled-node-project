3edc5cbbb13a9017460d245402f45498f93790a0
mgravemeyer <m.gravemeyer@icloud.com>
2022-02-20 16:50:57 +0100

    added basic client handling

--- index.ts
+++ index.ts
@@ -1,13 +1,36 @@
-const ws = require('ws');
 
-const server = new ws.Server({ port: 3000 });
+const WebSocketInstance = require('ws');
+const { uuid } = require('uuidv4')
 
-server.on('connection', (server) => {
-    server.on('message', (message) => {
-        let data = JSON.parse(message);
-        console.log('message data: ', data);
+const server = new WebSocketInstance.Server({ port: 3000 });
+const clients = new Map();
+
+server.on('connection', (ws) => {
+
+    const id = uuid();
+    const color = Math.floor(Math.random() * 360);
+    const metadata = {id, color};
+
+    clients.set(ws, metadata)
+
+    ws.on('message', (messageAsString) => {
+        try {
+            let message = JSON.parse(messageAsString);
+            let metadata = clients.get(ws);
+            message.sender = metadata.id;
+            const outbound = JSON.stringify(message);
+
+            [...clients.keys()].forEach((client) => {
+                console.log(client);
+                console.log(outbound);
+                client.send(outbound);
+            });
+            console.log('message data: ', message);
+        } catch(error) {
+            console.log(error)
+        }
     })
     server.on('closed', (code, reason) => {
-        console.log('closed | code: ', code, 'reason: ' , reason);
+        clients.delete(ws);
     })
 })
